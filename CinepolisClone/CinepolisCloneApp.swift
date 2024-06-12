3199bab9b1ed215a6bd88c848368283b9f627175
Ricardo Garcia <ricardogarcia.lopez@hotmail.com>
2024-06-12 16:37:47 -0600

    Initial Commit

---
+++ CinepolisClone/CinepolisCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinepolisCloneApp.swift
+//  CinepolisClone
+//
+//  Created by Ricardo Garcia on 12/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CinepolisCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
