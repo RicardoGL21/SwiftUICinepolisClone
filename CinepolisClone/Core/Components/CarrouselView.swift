ed426935d27f8b997dab15a080b4adcb3aa2214a
Ricardo Garcia <ricardogarcia.lopez@hotmail.com>
2024-06-13 10:57:11 -0600

    first commit

---
+++ CinepolisClone/Core/Components/CarrouselView.swift
@@ -0,0 +1,18 @@
+//
+//  CarrouselView.swift
+//  CinepolisClone
+//
+//  Created by Ricardo Garcia on 12/06/24.
+//
+
+import SwiftUI
+
+struct CarrouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarrouselView()
+}
---
+++ CinepolisClone/Core/MoviesView/MoviesView.swift
@@ -0,0 +1,18 @@
+//
+//  MoviesView.swift
+//  CinepolisClone
+//
+//  Created by Ricardo Garcia on 12/06/24.
+//
+
+import SwiftUI
+
+struct MoviesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoviesView()
+}
